55331224a1154924292851b35ba500cd3cfcf25e
Thibault <thibault.viravau@ensta-bretagne.org>
2015-03-09 14:08:32 +0100

    Initial commit

---
+++ app/src/main/java/as/swarmapp/lighttracker/Const.java
@@ -0,0 +1,20 @@
+package as.swarmapp.lighttracker;
+
+/**
+ * Created by asmodeeus on 09/03/15.
+ */
+public final class Const {
+    public static final String REGEX_SITE = "[a-zA-Z0-9][-a-zA-Z0-9]*(\\.[-a-zA-Z0-9]+)+(:[1-9][0-9]*)?"; // Vaguement fool-proof ...
+    public static final String REGEX_TOKEN = "[a-f0-9]+";
+
+    public static final String INVALID_ = "\"%s\" is invalid.";
+    public static final String ERROR_WITH_ = "Error with %s : ";
+    public static final String DOESN_T_MATCH = "it doesn't match the expected regular expression";
+    public static final String ERROR___ = ERROR_WITH_ + "\"%s\", " + DOESN_T_MATCH + " %s";
+
+    public static final String DONNEES = "adresse+token";
+
+    public static final String PREFERENCES = "LightTracker.preferences";
+    public static final String PREF_SITE = "site";
+    public static final String PREF_TOKEN = "token";
+}
---
+++ app/src/main/java/as/swarmapp/lighttracker/Principale.java
@@ -0,0 +1,106 @@
+package as.swarmapp.lighttracker;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.List;
+
+
+public class Principale extends ActionBarActivity {
+    private SharedPreferences 		sharedPref;
+    private EditText eSite;
+    private EditText eToken;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principale);
+        sharedPref 	= getSharedPreferences(Const.PREFERENCES, Context.MODE_PRIVATE);
+
+
+        eSite = (EditText) findViewById(R.id.Esite);
+        eToken = (EditText) findViewById(R.id.Etoken);
+        eSite.setText(sharedPref.getString(Const.PREF_SITE, ""));
+        eToken.setText(sharedPref.getString(Const.PREF_TOKEN, ""));
+
+        ((Button) findViewById(R.id.Bgo)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String donnees[] = donneesAreOk();
+                if (donnees != null) {
+                    sharedPref.edit()
+                            .putString(Const.PREF_SITE, donnees[0])
+                            .putString(Const.PREF_TOKEN, donnees[1])
+                            .apply();
+
+                    startActivity(new Intent(Principale.this, Track.class).putExtra(Const.DONNEES, donnees));
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_principale, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        switch(id){
+            case R.id.parametres:
+                return true;
+                //break;
+
+            case R.id.recuperation_donnees:
+                startActivity(new Intent(Principale.this, Recuperation.class));
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public String[] donneesAreOk(){
+        String site = eSite.getText().toString();
+        String token = eToken.getText().toString().toLowerCase();
+        String prefixe = "http://";
+
+        if (site.startsWith("http://")){
+            site = site.substring(7);
+
+        }else if (site.startsWith("https://")){
+            site = site.substring(8);
+            prefixe = "https://";
+        }
+
+        if (site.matches(Const.REGEX_SITE)){
+            if (token.matches(Const.REGEX_TOKEN)){
+                return new String[]{prefixe + site, token};
+
+            }else{
+                Toast.makeText(this, String.format(Const.INVALID_,getString(R.string.Ttoken)), Toast.LENGTH_SHORT).show();
+
+            }
+        }else{
+            Toast.makeText(this, String.format(Const.INVALID_,getString(R.string.Tsite)), Toast.LENGTH_SHORT).show();
+
+        }
+        return null;
+    }
+}
